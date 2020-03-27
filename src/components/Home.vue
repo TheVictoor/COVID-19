504a995f1d46d53cc6580ccb385dc317b5b30478
Jose Nilton Silveira de Sousa Filho <nilton.sousa@avanade.com>
2020-03-26 22:13:17 -0300

    Desenvolvimento de estrutura do card principal

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,14 @@
 <template>
-<div id="mainCard" class="container-fluid">
-    <h4>Brazil Test</h4>
-    <div id="victimsNumber" class="">
-
+<div id="mainCard" class="card">
+    <div class="card-header">
+        Status Report Covid-19
     </div>
+   <div class="card-body">
+       <div class="list-group-item">Card 1</div>
+       <div class="list-group-item">Card 2</div>
+       <div class="list-group-item">Card 3</div>
+   </div>
+   <div class="card-footer bg-transparent">Last Update </div>
 </div>
 </template>
 
@@ -14,5 +19,15 @@ export default {
 </script>
 
 <style scoped>
-
+.card{
+    width: 400px;
+    position: relative;
+    align-content: center;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+.card-footer {
+    font-size: 10px;
+}
 </style>

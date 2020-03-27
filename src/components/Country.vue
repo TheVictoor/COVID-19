e14d8003ff80e65f1d839270b0eb261660fda3fb
TheVictoor <victoorsp@gmail.com>
2020-03-26 21:48:35 -0300

    Componente para exibir nome do pais e bandeira

---
+++ src/components/Country.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="info-contai">
+		<p>
+			<span>
+				<img width="60px" :src="image" :alt="`bandeira do pais ${label}`">
+			</span>
+			<span>{{ label }}</span>
+		</p>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'country',
+	props: ['label', 'image'],
+	data() {
+		return {}
+	}
+}
+
+</script>
+
+<style scoped>
+
+</style>

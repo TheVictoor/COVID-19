740b314947d391654cd14b75784ada2f2bada633
TheVictoor <victoorsp@gmail.com>
2020-03-27 19:48:56 -0300

    Ajuste layout home

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
   <div id="app">
     <Home/>
-    <Information/>
   </div>
 </template>
 
 <script>
 import Home from './components/Home.vue';
-import Information from './components/Information.vue';
 
 export default {
   name: 'App',
   components: {
     Home,
-    Information
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  width: 100vw;
-  height: 100vh;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    background-color: #111;
+    width: 100vw;
+    height: 100vh;
+  }
 </style>

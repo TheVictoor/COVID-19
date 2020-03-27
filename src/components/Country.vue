0349c376fa89c20cf94e85e31c25c50b326451b5
TheVictoor <victoorsp@gmail.com>
2020-03-26 22:13:34 -0300

    Add country component

--- src/components/Country.vue
+++ src/components/Country.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="info-contai">
-		<p>
+		<div class="d-flex justify-content-center align-items-center">
 			<span>
-				<img width="60px" :src="image" :alt="`bandeira do pais ${label}`">
+				<img class="image-country" :src="image" :alt="`bandeira do pais ${label}`">
 			</span>
-			<span>{{ label }}</span>
-		</p>
+			<span class="label-country ml-4">{{ label }}</span>
+		</div>
 	</div>
 </template>
 
@@ -22,5 +22,15 @@ export default {
 </script>
 
 <style scoped>
+
+  .image-country {
+    width: 50px !important;
+    border-radius: 5px;
+  }
+
+	.label-country {
+		font-size: 24px;
+		font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+	}
 
 </style>

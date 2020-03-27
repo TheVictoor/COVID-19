19516f5873b180a0416b1df89e3bcf12f554ba2d
TheVictoor <victoorsp@gmail.com>
2020-03-27 20:34:26 -0300

    Ajustes layout

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,13 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif; 
-   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  width: 100vw;
-  height: 100vh;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    background-color: #111;
+    width: 100vw;
+    height: 100vh;
+  }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,9 +8,9 @@
 				</h3>
 			</div>
 			<div class="card-body">
-				<informativoComponent v-if="stats.confirmed" label='Confirmed' :number='stats.confirmed'/>
-				<informativoComponent v-if="stats.deaths" label='Deaths' :number='stats.deaths'/>
-				<informativoComponent v-if="stats.recovered" label='Recovered' :number='stats.recovered'/>
+				<informativoComponent class="mt-4" v-if="stats.confirmed" label='Confirmed' :number='stats.confirmed'/>
+				<informativoComponent class="mt-4" v-if="stats.deaths" label='Deaths' :number='stats.deaths'/>
+				<informativoComponent class="mt-4" v-if="stats.recovered" label='Recovered' :number='stats.recovered'/>
 			</div>
 			<div class="card-footer bg-transparent text-right">
 				{{ lastUpdate || "" }}
@@ -26,11 +26,7 @@ import services from "./../services";
 export default {
   name: "Home",
   components: {
-<<<<<<< HEAD
 		informativoComponent
-=======
-    informativoComponent,
->>>>>>> 740b314947d391654cd14b75784ada2f2bada633
   },
   data() {
     return {
@@ -73,106 +69,42 @@ export default {
       }, 15000)
     }
 
-		startVirus()
 		await changeData()
   },
 }
 
-function startVirus() {
-  var canvas, ctx, ban
-  canvas = document.getElementsByTagName("canvas")[0]
-  ctx = canvas.getContext("2d")
-  canvas.width = canvas.height = 500
-  main()
-
-  function main() {
-    var tim
-    ctx.globalCompositeOperation = "source-over"
-    ctx.fillStyle = "rgb(0,0,0)"
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-    tim = new Date().getTime() / 5000
-    ban = 0
-    rus(200, 200, 1, tim, 0)
-    requestAnimationFrame(main)
-  }
-
-  function rus(x, y, s, r, k) {
-    var a, px, py, r1, r2, han
-    han = 50
-    if (k > 2) {
-      a = (k * 5 + 200) % 360
-      ctx.globalCompositeOperation = "lighter"
-      ctx.fillStyle = "hsla(" + a + ",60%,50%,0.1)"
-      ctx.beginPath()
-      ctx.arc(x, y, han * s * 2, 0, Math.PI * 2, 0)
-      ctx.fill()
-      ctx.globalCompositeOperation = "source-over"
-      ctx.fillStyle = "hsl(" + a + ",60%,30%)"
-      ctx.beginPath()
-      ctx.arc(x, y, han * s, 0, Math.PI * 2, 0)
-      ctx.fill()
-    }
-    ban++
-    if (s < 0.3) return
-    k++
-    r1 = 0.87
-    r2 = 1 - r1
-    r += ban
-    px = Math.cos(r)
-    py = Math.sin(r)
-    rus(x + px * han * r2 * s, y + py * han * r2 * s, s * r1, r * 1.2, k)
-    r += ban
-    px = Math.cos(r)
-    py = Math.sin(r)
-    r1 = 0.82
-    rus(x + px * han * (1 + r1) * s, y + py * han * (1 + r1) * s, s * r1, -r, k)
-  }
-}
 </script>
 
 <style scoped>
-.container-dash {
-  width: 100vw;
-  height: 100vh;
-}
 
-.card-style {
-  width: 300px;
-}
+	.container-dash {
+		width: 100vw;
+		height: 100vh;
+	}
 
-.card-footer {
-  font-size: 10px;
-}
+	.card-style {
+		width: 400px;
+		background: rgba(255, 255, 255, .9);
+	}
 
-.image-country {
-  width: 70px;
-  border-radius: 10px;
-  position: relative;
-  top: -35px;
-}
+	.card-footer {
+		font-size: 10px;
+	}
 
-.label-country {
-  position: relative;
-  top: -25px;
-}
+	.image-country {
+		width: 70px;
+		border-radius: 10px;
+		position: relative;
+		top: -35px;
+	}
 
-.card-header {
-  height: 80px;
-}
+	.label-country {
+		position: relative;
+		top: -25px;
+	}
 
-.card-style {
-  background: rgba(255, 255, 255, 0.8);
-  position: relative;
-  top: -50vh;
-}
+	.card-header {
+		height: 80px;
+	}
 
-canvas {
-  position: relative;
-  top: 50%;
-  left: 50%;
-  margin-left: -50vmin;
-  width: 100vmin;
-  height: 100vmin;
-  overflow: hidden;
-}
 </style>

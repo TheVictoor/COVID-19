9577949f92ad14bb5469ccfeb6c9b5638d7a4422
Jose Nilton Silveira de Sousa Filho <nilton.sousa@avanade.com>
2020-03-26 21:27:07 -0300

    Adicionando bootstrap e incluindo components

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="mainCard" class="container-fluid">
+    <h4>Brazil Test</h4>
+    <div id="victimsNumber" class="">
+
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name:'Home'
+}
+</script>
+
+<style scoped>
+
+</style>

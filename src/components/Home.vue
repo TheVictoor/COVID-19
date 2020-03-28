26bb4aa36bd1ec864e13ad7e61a7e2d50015ae77
TheVictoor <victoorsp@gmail.com>
2020-03-27 21:53:09 -0300

    dbClick na bandeira para mudar pais

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,39 @@
 <template>
-	<div class="container-dash d-flex justify-content-center align-items-center">
-		<div class="card card-style">
-			<div class="card-header text-center">
-				<img class="image-country" v-if="country.flag" :src="country.flag" :alt="`bandeira do pais ${country.name}`">
-				<h3 class="label-country">
-					{{ country.name || "" }}
-				</h3>
-			</div>
-			<div class="card-body">
-				<informativoComponent class="mt-4" v-if="stats.confirmed" label='Confirmed' :number='stats.confirmed'/>
-				<informativoComponent class="mt-4" v-if="stats.deaths" label='Deaths' :number='stats.deaths'/>
-				<informativoComponent class="mt-4" v-if="stats.recovered" label='Recovered' :number='stats.recovered'/>
-			</div>
-			<div class="card-footer bg-transparent text-right">
-				{{ lastUpdate || "" }}
-			</div>
-		</div>
-	</div>
+  <div class="container-dash d-flex justify-content-center align-items-center">
+    <div class="card card-style mx-2">
+      <div class="card-header text-center">
+        <img
+          @dblclick="updateNext()"
+          class="image-country"
+          v-if="country.flag"
+          :src="country.flag"
+          :alt="`bandeira do pais ${country.name}`"
+        />
+        <h3 class="label-country">{{ country.name || "" }}</h3>
+      </div>
+      <div class="card-body">
+        <informativoComponent
+          class="mt-4"
+          v-if="stats.confirmed"
+          label="Confirmed"
+          :number="stats.confirmed"
+        />
+        <informativoComponent
+          class="mt-4"
+          v-if="stats.deaths"
+          label="Deaths"
+          :number="stats.deaths"
+        />
+        <informativoComponent
+          class="mt-4"
+          v-if="stats.recovered"
+          label="Recovered"
+          :number="stats.recovered"
+        />
+      </div>
+      <div class="card-footer bg-transparent text-right">{{ lastUpdate || "" }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -35,7 +52,9 @@ export default {
         flag: null,
       },
       stats: {},
-      lastUpdate: null,
+			lastUpdate: null,
+			timeoutId: null,
+			countryIndex: 0,
     }
   },
   methods: {
@@ -47,64 +66,68 @@ export default {
       )} at ${fill(date.getHours())}:${fill(date.getMinutes())}`
       return extracted
     },
+    updateNext() {
+      clearTimeout(this.timeoutId);
+      this.startTimerUpdateStats();
+    },
+		async startTimerUpdateStats(){
+      const countries = process.env.VUE_APP_COUNTRIES.split(",")
+			const changeData = async () => {
+				const country = countries[this.countryIndex]
+				const flag = await services.country.get(country)
+				const stats = await services.covid.get(country)
+
+				this.lastUpdate = this.extractDate(stats.lastUpdate)
+				this.country.name = country
+				this.country.flag = flag
+				this.stats = stats
+
+				this.countryIndex++
+				if (this.countryIndex === countries.length) this.countryIndex = 0
+
+				this.timeoutId = setTimeout(function() {
+					changeData();
+				}, 15000)
+			}
+
+			await changeData()
+		}
   },
   async created() {
-    let index = 0
-    const countries = process.env.VUE_APP_COUNTRIES.split(",")
-    const changeData = async () => {
-      const country = countries[index]
-      const flag = await services.country.get(country)
-      const stats = await services.covid.get(country)
-
-      this.lastUpdate = this.extractDate(stats.lastUpdate)
-      this.country.name = country
-      this.country.flag = flag
-      this.stats = stats
-
-      index++
-      if (index === countries.length) index = 0
-
-      setTimeout(function() {
-        changeData()
-      }, 15000)
-    }
-
-		await changeData()
+		this.startTimerUpdateStats();
   },
 }
 
 </script>
 
 <style scoped>
+.container-dash {
+  width: 100vw;
+  height: 100vh;
+}
 
-	.container-dash {
-		width: 100vw;
-		height: 100vh;
-	}
+.card-style {
+  width: 400px;
+  background: rgba(255, 255, 255, 0.9);
+}
 
-	.card-style {
-		width: 400px;
-		background: rgba(255, 255, 255, .9);
-	}
+.card-footer {
+  font-size: 10px;
+}
 
-	.card-footer {
-		font-size: 10px;
-	}
+.image-country {
+  width: 70px;
+  border-radius: 10px;
+  position: relative;
+  top: -35px;
+}
 
-	.image-country {
-		width: 70px;
-		border-radius: 10px;
-		position: relative;
-		top: -35px;
-	}
+.label-country {
+  position: relative;
+  top: -25px;
+}
 
-	.label-country {
-		position: relative;
-		top: -25px;
-	}
-
-	.card-header {
-		height: 80px;
-	}
-
+.card-header {
+  height: 80px;
+}
 </style>

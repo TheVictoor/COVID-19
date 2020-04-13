6ba01f15e1380b08610eb37315837c2b2899d8d4
TheVictoor <victoorsp@gmail.com>
2020-04-12 23:04:03 -0300

    Ajuste install experience

--- public/install.js
+++ public/install.js
@@ -1,27 +1,16 @@
 
 'use strict';
 
-let deferredInstallPrompt = null;
-const installButton = document.getElementById('butInstall');
-
-installButton.addEventListener('click', (evt) => {
-  deferredInstallPrompt.prompt();
-  evt.srcElement.setAttribute('hidden', true);
-
-  deferredInstallPrompt.userChoice
+window.addEventListener('beforeinstallprompt', (evt) => {
+  evt.prompt();
+  evt.userChoice
     .then((choice) => {
       if (choice.outcome === 'accepted') {
         console.log('User accepted the A2HS prompt', choice);
       } else {
         console.log('User dismissed the A2HS prompt', choice);
       }
-      deferredInstallPrompt = null;
     });
-});
-
-window.addEventListener('beforeinstallprompt', (evt) => {
-  deferredInstallPrompt = evt;
-  installButton.removeAttribute('hidden');
 });
 
 window.addEventListener('appinstalled', (evt) => {

1c4878a16062ef2a38c1790cd73da514933d894e
TheVictoor <victoorsp@gmail.com>
2020-04-12 23:17:44 -0300

    PWA action to install

--- public/install.js
+++ public/install.js
@@ -1,16 +1,27 @@
 
 'use strict';
 
-window.addEventListener('beforeinstallprompt', (evt) => {
-  evt.prompt();
-  evt.userChoice
+let deferredInstallPrompt = null;
+const installButton = document.getElementById('butInstall');
+installButton.addEventListener('click', (evt) => {
+  deferredInstallPrompt.prompt();
+  evt.srcElement.setAttribute('hidden', true);
+
+  deferredInstallPrompt.userChoice
     .then((choice) => {
       if (choice.outcome === 'accepted') {
         console.log('User accepted the A2HS prompt', choice);
       } else {
         console.log('User dismissed the A2HS prompt', choice);
       }
+      deferredInstallPrompt = null;
     });
+
+});
+
+window.addEventListener('beforeinstallprompt', (evt) => {
+  deferredInstallPrompt = evt;
+  installButton.removeAttribute('hidden');
 });
 
 window.addEventListener('appinstalled', (evt) => {
